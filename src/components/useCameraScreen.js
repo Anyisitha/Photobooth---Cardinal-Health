01eef93bd9c7e25cdf4b25a424f3fe069bb12419
Anyi Lozano <anyisithalozano12@gmail.com>
2022-10-03 07:41:23 -0500

    new devices

--- src/components/useCameraScreen.js
+++ src/components/useCameraScreen.js
@@ -7,7 +7,7 @@ const useCameraScreen = () => {
      */
     const changeCamera = (cameraPhoto) => {
         
-        console.log(cameraPhoto.inputVideoDeviceInfos)
+        console.log(cameraPhoto.getInputVideoDeviceInfos())
     }
 
     changeCamera.propTypes = {
